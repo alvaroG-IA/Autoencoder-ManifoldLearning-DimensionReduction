e8f197ca9efb644c35b62a6e6f4cbfb092f16be7
alvaroG-IA <alvarogarciavelasco1212@gmail.com>
2025-10-08 13:27:38 +0200

    Interfaz 'Autoencoder.py'

---
+++ packages/Autoencoder.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Autoencoder:
+    def __init__(self, epochs: int, loss_threshold: float):
+        self.epochs = epochs
+        self.loss_threshold = loss_threshold
+    
+    def fit(self, data: np.narray):
+        pass
+
+    def transform(self, data: np.narray):
+        pass
